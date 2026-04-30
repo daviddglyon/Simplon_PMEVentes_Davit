20dcfbad6c229f8c39da3245daea67383fd958db
daviddglyon <davidgogia@ymail.com>
2026-04-30 00:40:53 +0000

    création bd et deux figures pour des nouveaux graphiques

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
 import plotly.express as px
 import pandas as pd
+import sqlite3
 
-données = pd.read_csv('https://docs.google.com/spreadsheets/d/e/2PACX-1vSC4KusfFzvOsr8WJRgozzsCxrELW4G4PopUkiDbvrrV2lg0S19-zeryp02MC9WYSVBuzGCUtn8ucZW/pub?output=csv')
+con = sqlite3.connect("SQLite.db")
+ventes = pd.read_sql_query("SELECT produit, sum(qte) ventes_par_produit  FROM ventes GROUP BY produit;", con)
+ca = pd.read_sql_query("SELECT produit, sum(qte * prix) CA FROM ventes GROUP by produit;", con)
+con.close()
 
-figure = px.pie(données, values='qte', names='region', title='quantité vendue par région')
+figure_ventes_par_produit = px.pie(ventes, values='ventes_par_produit', names='produit', title='quantité vendue par produit')
+figure_ventes_par_produit.write_html('ventes-par-produit.html')
+print('ventes-par-produit.html généré avec succès !')
 
-figure.write_html('ventes-par-region.html')
+figure_ca_par_produit     = px.pie(ca, values='CA', names='produit', title='CA par produit')
+figure_ca_par_produit.write_html('ca-par-produit.html')
+print('ca-par-produit.html généré avec succès !')
 
-print('ventes-par-région.html généré avec succès !')
+
